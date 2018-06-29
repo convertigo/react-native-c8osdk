8e1488260450033ebc91db9d8500efdc8f561ceb
Grimont <grimont.c@gmail.com>
2018-06-29 16:40:29 +0200

    add a clean task to package.json and remove .js .js.map .flow.js and .d.ts from git

--- src/all.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./c8o";
-export * from "./c8oSettings";
-export * from "./c8oLogLevel";
-export * from "./c8oLogger";
-export * from "./c8oLocalCache";
-export * from "./priority";
--- src/c8oLogger.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class C8oLogger {
-    constructor();
-    fatal(message: string): Promise<any>;
-    error(message: string): Promise<any>;
-    warn(message: string): Promise<any>;
-    info(message: string): Promise<any>;
-    debug(message: string): Promise<any>;
-    trace(message: string): Promise<any>;
-}
